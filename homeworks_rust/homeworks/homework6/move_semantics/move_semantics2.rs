0e87c9f72153751c6a7afe75edb0e385aa942b98
Jake <jkktom@gmail.com>
2024-04-03 16:56:49 +0900

    until semantics3

--- homeworks_rust/homeworks/homework6/move_semantics/move_semantics2.rs
+++ homeworks_rust/homeworks/homework6/move_semantics/move_semantics2.rs
@@ -2,12 +2,11 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
     let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
+    // let _vec2 = vec0.clone(); this is the solution 1 and for below line to use fill_vec(_vec2)
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,12 +16,12 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> { // this is solution 3
+    let mut vec:  Vec<i32> = vec.to_vec();
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec.to_vec()
 }
